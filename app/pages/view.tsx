37329811b4722a855eebca9a68b4d555a23213d3
Andy Tang <andy.tang@action.nl>
2022-12-14 11:27:50 +0100

    Bootstrap pages

---
+++ app/pages/view.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Stake for steak</title>
+        <meta name="description" content="Stake for stake" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>View</h1>
+
+        <p className={styles.description}>View your stake here</p>
+      </main>
+    </div>
+  );
+}

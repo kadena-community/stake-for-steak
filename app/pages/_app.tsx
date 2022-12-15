c9e4be7637383dfb1dd96882e404c572cd531136
Andy Tang <andy.tang@action.nl>
2022-12-15 16:23:19 +0100

    Add home button

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -6,7 +6,12 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="h-screen bg-slate-800">
       <Component {...pageProps} />
-      <footer className="fixed bottom-0">
+      <footer className="fixed bottom-0 flex flex-row">
+        <Link href="/">
+          <span className="block p-2 m-2 bg-slate-700 rounded-md text-slate-100">
+            overview
+          </span>
+        </Link>
         <Link href="/add-stake">
           <span className="block p-2 m-2 bg-slate-700 rounded-md text-slate-100">
             add stake

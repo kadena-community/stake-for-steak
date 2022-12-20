a05529df88fc9d8242fd23a1a9a474315dcff93f
Andy Tang <andy.tang@action.nl>
2022-12-20 17:21:42 +0100

    Add consent modal

--- app/hooks/use-fund.ts
+++ app/hooks/use-fund.ts
@@ -1,47 +1,58 @@
 import useSWRMutation from "swr/mutation";
 import { Pact, signWithChainweaver } from "@kadena/client";
 import { getEscrowId } from "../utils/get-escrow-id";
+import { useContext } from "react";
+import { ConsentModalContext } from "../components/consent-context";
 
 interface FundInput {
   staker: string;
   keys: string[];
 }
-const fundStake = async (name: string, { arg }: { arg: FundInput }) => {
-  const { staker, keys } = arg;
-  const stake = await (Pact.modules as any)["free.stake-for-steak"]
-    ["get-stake"](name)
-    .local(
+const fundStake =
+  (open: Function) =>
+  async (name: string, { arg }: { arg: FundInput }) => {
+    const modal = await open(["GAS", "TRANSFER"]);
+    console.log("modal", modal);
+
+    const { staker, keys } = arg;
+    const stake = await (Pact.modules as any)["free.stake-for-steak"]
+      ["get-stake"](name)
+      .local(
+        "https://api.testnet.chainweb.com/chainweb/0.0/testnet04/chain/1/pact"
+      );
+
+    const escrowId = await getEscrowId(name, stake.result.data.owner);
+
+    const command = (Pact.modules as any)["free.stake-for-steak"]
+      ["fund-stake"](name, staker, () => `(read-keyset 'keyset)`)
+      .addData({
+        keyset: { pred: "keys-all", keys },
+      })
+      .addCap("coin.GAS" as any, keys[0])
+      .addCap(
+        "coin.TRANSFER" as any,
+        keys[0],
+        staker,
+        escrowId,
+        stake.result.data.stake
+      )
+      .setMeta({ sender: staker }, "testnet04");
+
+    await signWithChainweaver(command);
+
+    const res = await command.send(
       "https://api.testnet.chainweb.com/chainweb/0.0/testnet04/chain/1/pact"
     );
-
-  const escrowId = await getEscrowId(name, stake.result.data.owner);
-
-  const command = (Pact.modules as any)["free.stake-for-steak"]
-    ["fund-stake"](name, staker, () => `(read-keyset 'keyset)`)
-    .addData({
-      keyset: { pred: "keys-all", keys },
-    })
-    .addCap("coin.GAS" as any, keys[0])
-    .addCap(
-      "coin.TRANSFER" as any,
-      keys[0],
-      staker,
-      escrowId,
-      stake.result.data.stake
-    )
-    .setMeta({ sender: staker }, "testnet04");
-
-  await signWithChainweaver(command);
-
-  const res = await command.send(
-    "https://api.testnet.chainweb.com/chainweb/0.0/testnet04/chain/1/pact"
-  );
-  console.log(res);
-  return res;
-};
+    console.log(res);
+    return res;
+  };
 
 export function useFund(name: string | undefined) {
-  const { data, error, isMutating, trigger } = useSWRMutation(name, fundStake);
+  const { open } = useContext(ConsentModalContext);
+  const { data, error, isMutating, trigger } = useSWRMutation(
+    name,
+    fundStake(open)
+  );
   return {
     transaction: data,
     isFunding: isMutating,
--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,28 +1,37 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from "next/link";
+import { ConsentModalContext } from "../components/consent-context";
+import { Modal } from "../components/modal";
+import { useModal } from "../hooks/use-modal";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const modal = useModal();
+  console.log("modal", modal);
+
   return (
     <div className="h-screen bg-slate-800 kadena-bg">
-      <Component {...pageProps} />
-      <footer className="fixed bottom-0 flex flex-row">
-        <Link href="/">
-          <span className="block p-2 m-2 button rounded-md text-slate-100">
-            overview
-          </span>
-        </Link>
-        <Link href="/add-stake">
-          <span className="block p-2 m-2 button rounded-md text-slate-100">
-            add stake
-          </span>
-        </Link>
-        <Link href="/create">
-          <span className="block p-2 m-2 button rounded-md text-slate-100">
-            create stake
-          </span>
-        </Link>
-      </footer>
+      <ConsentModalContext.Provider value={modal}>
+        <Component {...pageProps} />
+        <Modal />
+        <footer className="fixed bottom-0 flex flex-row">
+          <Link href="/">
+            <span className="block p-2 m-2 button rounded-md text-slate-100">
+              overview
+            </span>
+          </Link>
+          <Link href="/add-stake">
+            <span className="block p-2 m-2 button rounded-md text-slate-100">
+              add stake
+            </span>
+          </Link>
+          <Link href="/create">
+            <span className="block p-2 m-2 button rounded-md text-slate-100">
+              create stake
+            </span>
+          </Link>
+        </footer>
+      </ConsentModalContext.Provider>
     </div>
   );
 }
